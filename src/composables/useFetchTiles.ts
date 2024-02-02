8a8c29398de7a7ba567ca1cee6e60f1b15d18cdb
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-02-03 00:02:43 +0900

    add access token for cs2

--- src/composables/useFetchTiles.ts
+++ src/composables/useFetchTiles.ts
@@ -1,7 +1,7 @@
 export const useFetchTerrainTiles = async (zoom: number, x: number, y: number) => {
   const config = useRuntimeConfig()
   const mapbox = useMapbox()
-  const token = mapbox.value.settings.gridInfo === 'cs1' ? config.public.token : mapbox.value.settings.accessToken
+  const token = mapbox.value.settings.accessToken || config.public.token
   const url = `https://api.mapbox.com/v4/mapbox.mapbox-terrain-dem-v1/${zoom}/${x}/${y}@2x.pngraw?access_token=${token}`
   const { data, error } = await useFetch<Blob>(url, { key: `terrain-dem:${zoom}-${x}-${y}` })
   return { data, error }
@@ -10,7 +10,7 @@ export const useFetchTerrainTiles = async (zoom: number, x: number, y: number) =
 export const useFetchVectorTiles = async (zoom: number, x: number, y: number) => {
   const config = useRuntimeConfig()
   const mapbox = useMapbox()
-  const token = mapbox.value.settings.gridInfo === 'cs1' ? config.public.token : mapbox.value.settings.accessToken
+  const token = mapbox.value.settings.accessToken || config.public.token
   const url = `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/${zoom}/${x}/${y}.vector.pbf?access_token=${token}`
   const { data, error } = await useFetch<Blob>(url, { key: `mapbox-streets-v8:${zoom}-${x}-${y}` })
   return { data, error }
