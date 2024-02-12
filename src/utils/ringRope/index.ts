5a5315190f23e1bc856bb76759388228d347aa44
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-02-12 17:21:44 +0900

    update ring rope

--- src/utils/ringRope/index.ts
+++ src/utils/ringRope/index.ts
@@ -1,34 +1,53 @@
 import { Mesh, MeshMaterial } from '@pixi/mesh'
 import type { IPoint, Renderer, Texture } from '@pixi/core'
-import { RingRopeGeometry } from '~/utils/ringRope/ringRopeGeometry'
-
+import type { CanvasRenderer } from '@pixi/canvas-renderer'
+import { RingRopeGeometry } from './ringRopeGeometry'
+import './fixPadding'
 
 export class RingRope extends Mesh {
   public autoUpdate: boolean
 
-  constructor(texture: Texture, points: IPoint[]) {
-    const ropeGeometry = new RingRopeGeometry(texture.height, points)
-    const meshMaterial = new MeshMaterial(texture)
+  _scale: number
 
-    // offset: adjusting the position of the mesh
+  constructor(texture: Texture, points: IPoint[], scale = 0.5) {
+    const ropeGeometry = new RingRopeGeometry(texture.height, points, scale)
+    const meshMaterial = new MeshMaterial(texture)
 
     super(ropeGeometry, meshMaterial)
 
+    this._scale = scale
+
     /**
-         * re-calculate vertices by rope points each frame
-         * @member {boolean}
-         */
+     * re-calculate vertices by rope points each frame
+     * @member {boolean}
+     */
     this.autoUpdate = true
   }
 
   _render(renderer: Renderer): void {
     const geometry: RingRopeGeometry = this.geometry as any
 
-    if (this.autoUpdate || geometry._width !== this.shader.texture.height) {
-      geometry._width = this.shader.texture.height
+    if (this.autoUpdate || geometry._width !== this.shader.texture.height * this._scale) {
+      geometry._width = this.shader.texture.height * this._scale
       geometry.update()
     }
 
     super._render(renderer)
+  }
+
+  _renderCanvas(renderer: CanvasRenderer): void {
+    const geometry: RingRopeGeometry = this.geometry as any
+    if (this.autoUpdate || geometry._width !== this.shader.texture.height + this._scale) {
+      geometry._width = this.shader.texture.height * this._scale
+      geometry.update()
+    }
+
+    if (this.shader.update) {
+      this.shader.update()
+    }
+
+    this.calculateUvs()
+
+    this.material._renderCanvas(renderer, this)
   }
 }
