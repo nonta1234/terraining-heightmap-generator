cf4456f3f507e0dce442f4be3ab1e33ee896ebd0
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-05-12 04:29:15 +0900

    remove comment

--- src/composables/useFetchTiles.ts
+++ src/composables/useFetchTiles.ts
@@ -1,34 +1,5 @@
 import { $fetch, FetchError } from 'ofetch'
 
-/**
-export const useFetchTerrainTiles = async (zoom: number, x: number, y: number) => {
-  const nuxtApp = useNuxtApp()
-  const config = useRuntimeConfig()
-  const mapbox = useMapbox()
-  const token = mapbox.value.settings.accessToken || config.public.token
-  const url = `https://api.mapbox.com/v4/mapbox.mapbox-terrain-dem-v1/${zoom}/${x}/${y}@2x.pngraw?access_token=${token}`
-  const { data, error } = await useFetch<Blob>(url, {
-    key: `terrain_dem_v1:${zoom}_${x}_${y}`,
-    getCachedData: key => nuxtApp.payload.data[key] || nuxtApp.static.data[key],
-  })
-  return { data, error }
-}
-
-export const useFetchVectorTiles = async (zoom: number, x: number, y: number) => {
-  const nuxtApp = useNuxtApp()
-  const config = useRuntimeConfig()
-  const mapbox = useMapbox()
-  const token = mapbox.value.settings.accessToken || config.public.token
-  const url = `https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/${zoom}/${x}/${y}.vector.pbf?access_token=${token}`
-  const { data, error } = await useFetch<Blob>(url, {
-    key: `mapboxs_treets_v8:${zoom}_${x}_${y}`,
-    getCachedData: key => nuxtApp.payload.data[key] || nuxtApp.static.data[key],
-  })
-  return { data, error }
-}
-*/
-
-/** */
 export const useFetchTerrainTiles = async (zoom: number, x: number, y: number, token: string) => {
   const url = `https://api.mapbox.com/v4/mapbox.mapbox-terrain-dem-v1/${zoom}/${x}/${y}@2x.pngraw?access_token=${token}`
   let data: Blob | undefined
