958c81b9b01d1f7b15a88f44601602af4ab86f32
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-05-12 21:24:58 +0900

    update getCitiesMap

--- src/utils/getCitiesMap.ts
+++ src/utils/getCitiesMap.ts
@@ -1,29 +1,38 @@
-import type { MapType } from '~/types/types'
+import GetCitiesMapWorker from '~/workers/getCitiesMapWorker.ts?worker'
+import type { MapType, Settings, GenerateMapOption } from '~/types/types'
+
+const getCitiesMapData = (mapType: MapType, settings: Settings, heightmap: Float32Array, waterMap: Float32Array, waterWayMap: Float32Array) => {
+  const data: GenerateMapOption = {
+    mapType,
+    settings,
+  }
+  const generateMapOption = JSON.parse(JSON.stringify(data))
+  const worker = new GetCitiesMapWorker()
+  const result = new Promise<Uint8Array>((resolve) => {
+    worker.addEventListener('message', (e) => {
+      if (e.data) {
+        resolve(e.data)
+        worker.terminate()
+      }
+    }, { once: true })
+  })
+  worker.postMessage({ body: generateMapOption, heightmap, waterMap, waterWayMap }, [heightmap.buffer, waterMap.buffer, waterWayMap.buffer])
+  return result
+}
 
 /**
- * Start Worker, get heightmap and watermap, synthesize, and return heightmap of Uint8Array
- * @param mapType
+ * Generate CitiesMap by combining heightmap, waterMap and waterWayMap.
+ * @param mapType MapType
+ * @param heightmap Float32Array
+ * @param waterMap Float32Array
+ * @param waterWayMap Float32Array
  * @returns Promise\<Uint8Array\>
  */
-export const getCitiesMap = async (mapType: MapType) => {
+export const getCitiesMap = async (mapType: MapType = 'cs1', heightmap: Float32Array, waterMap: Float32Array, waterWayMap: Float32Array) => {
   try {
-    const mapbox = useMapbox()
-    const config = useRuntimeConfig()
-    const token = mapType === 'cs1' ? config.public.token : (mapbox.value.settings.accessToken || config.public.token)
-    // const scaleFactor = mapbox.value.settings.gridInfo === 'cs2' ? (mapbox.value.settings.elevationScale / 65535) : 0.015625
-
-    const waterCanvases = []
-    const canvasesData = useState<OffscreenCanvas[]>('canvases').value
-    for (let i = 1; i < 4; i++) {
-      waterCanvases.push(canvasesData[i])
-    }
-    const tileCanvas = canvasesData[0]
-
-
-    await getHeightMap(mapbox.value.settings, token, mapType)
-
-
-    return null
+    const { settings } = useMapbox().value
+    const result = await getCitiesMapData(mapType, settings, heightmap, waterMap, waterWayMap)
+    return result
   } catch (error) {
     console.error('An error occurred in getCitiesMap:', error)
     throw error
