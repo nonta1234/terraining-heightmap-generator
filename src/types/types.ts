89c5678acb3b4c284547a459e01a06b35d3f2ea0
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-07-20 22:30:40 +0900

    update MapSpec

--- src/types/types.ts
+++ src/types/types.ts
@@ -23,8 +23,10 @@ export type GridSpec = {
 }
 
 export type MapSpec = {
+  name:        string;
   defaultSize: number | undefined;
   defaultRes:  number;
+  defaultEs:   number;
   resolutions: number[];
   correction:  0 | 1;
   grid:        GridSpec;
