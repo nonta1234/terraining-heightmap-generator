38cf7436d937945af08cf7702cf693377ad0b1b5
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-07-28 14:33:30 +0900

    remove event-bus

--- src/composables/useEventBus.ts
+++ src/composables/useEventBus.ts
@@ -2,15 +2,15 @@ import mitt from 'mitt'
 import type { LngLat } from '~/types/types'
 
 type ApplicationEvents = {
-  'map:changeLngLat': LngLat;
-  'map:changeMapSize': number;
-  'map:leModal': boolean | undefined;
-  'map:cpModal': boolean | undefined;
-  'map:miModal': boolean | undefined;
-  'modal:changeLittArray': void;
-  'modal:pointDragged': void;
-  'debug:operate': void;
-};
+  'map:changeLngLat': LngLat
+  'map:changeMapSize': number
+  'map:miModal': boolean | undefined
+  'modal:changeLittArray': void
+  'modal:pointDragged': void
+  'panel:tabChange': number
+  'panel:updateHeight': void
+  'debug:operate': void
+}
 
 const emitter = mitt<ApplicationEvents>()
 
