84eb61de00a285e664d0813c42ea5221258b653b
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-09-06 22:48:17 +0900

    add ocean map

--- heightmap_lib/src/lib.rs
+++ heightmap_lib/src/lib.rs
@@ -3,10 +3,12 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub struct Heightmap {
     heightmap: Vec<f32>,
+    oceanmap: Vec<f32>,
     watermap: Vec<f32>,
     waterwaymap: Vec<f32>,
     result: Vec<f32>,
     pub pointer_to_heightmap: *const f32,
+    pub pointer_to_oceanmap: *const f32,
     pub pointer_to_watermap: *const f32,
     pub pointer_to_waterwaymap: *const f32,
     pub pointer_to_result: *const f32,
@@ -18,10 +20,12 @@ impl Heightmap {
     pub fn new() -> Self {
         Self {
             heightmap: Vec::new(),
+            oceanmap: Vec::new(),
             watermap: Vec::new(),
             waterwaymap: Vec::new(),
             result: Vec::new(),
             pointer_to_heightmap: std::ptr::null(),
+            pointer_to_oceanmap: std::ptr::null(),
             pointer_to_watermap: std::ptr::null(),
             pointer_to_waterwaymap: std::ptr::null(),
             pointer_to_result: std::ptr::null(),
@@ -32,6 +36,10 @@ impl Heightmap {
         self.heightmap = Vec::with_capacity(length);
         unsafe { self.heightmap.set_len(length) }
         self.pointer_to_heightmap = self.heightmap.as_ptr();
+
+        self.oceanmap = Vec::with_capacity(length);
+        unsafe { self.oceanmap.set_len(length) }
+        self.pointer_to_oceanmap = self.oceanmap.as_ptr();
 
         self.watermap = Vec::with_capacity(length);
         unsafe { self.watermap.set_len(length) }
@@ -46,10 +54,13 @@ impl Heightmap {
         self.pointer_to_result = self.result.as_ptr();
     }
 
-    pub fn combine_heightmaps(&mut self, stream_depth: usize) {
+    pub fn combine_heightmaps(&mut self, water_depth: usize, stream_depth: usize) {
         for i in 0..(self.heightmap.len()) {
-            let h = if self.heightmap[i] == 0.0 { self.watermap[i] } else { self.heightmap[i] };
-            self.result[i] = h - ((1.0 - self.waterwaymap[i]) * (stream_depth as f32));
+            let h = if self.heightmap[i] == 0.0 { self.oceanmap[i] } else { self.heightmap[i] };
+            let water_effect = ((1.0 - self.watermap[i]) * (water_depth as f32)).max(
+                (1.0 - self.waterwaymap[i]) * (stream_depth as f32)
+            );
+            self.result[i] = h - water_effect;
         }
     }
 
