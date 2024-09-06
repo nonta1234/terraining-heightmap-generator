c0739a44e95fd0d07d79059d021f28930ba9b5d2
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-09-06 22:56:55 +0900

    test

--- src/utils/elevation.ts
+++ src/utils/elevation.ts
@@ -13,6 +13,45 @@ export const remToPx = (rem: number) => {
   return rem * parseFloat(fontSize)
 }
 
+/**
+ * Converts a feet and inches string to meters.
+ * @param feetString A string in feet and inches format (e.g., "16'3\"")
+ * @returns The equivalent value in meters
+ */
+function feetToMeters(feetString: string): number {
+  const match = feetString.match(/(\d+)'(\d+)"/)
+  if (match) {
+    const feet = parseInt(match[1], 10)
+    const inches = parseInt(match[2], 10)
+    const meters = (feet * 0.3048) + (inches * 0.0254)
+    return meters
+  } else {
+    return 5
+  }
+}
+
+/**
+* Determines if the value is in meters or feet and converts to meters if in feet.
+* @param value A number (meters) or a string (feet and inches)
+* @returns The value in meters
+*/
+export const convertToMeters = (value: string | number) => {
+  if (typeof value === 'number') {
+    // Return the value as-is if it's already in meters
+    return value
+  } else if (typeof value === 'string') {
+    // Convert feet and inches to meters if the value is a string
+    try {
+      return feetToMeters(value)
+    } catch (error) {
+      console.error((error as Error).message)
+      return 5 // Return default value if an error occurs
+    }
+  } else {
+    throw new Error('Invalid value type')
+  }
+}
+
 export const adjustElevation = (maxHeight: number) => {
   const mapbox = useMapbox()
   if (mapbox.value.settings.type !== 'manual') {
