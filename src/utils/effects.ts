f68774f2433170d195c24dc9f5236912b584aafb
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-09-28 08:11:04 +0900

    update effects

--- src/utils/effects.ts
+++ src/utils/effects.ts
@@ -1,65 +1,115 @@
-import { allocate_memory, free_memory, gaussian_blur, unsharp_mask, type InitOutput } from '~~/effects_lib/pkg'
+import { allocate_memory, free_memory, gaussian_blur, unsharp_mask, noise as gen_noise, type InitOutput } from '~~/effects_lib/pkg'
 
-export const gaussianBlur = (instance: InitOutput | undefined, data: Float32Array, radius: number, blend: number) => {
-  if (!instance) {
-    throw new Error('gaussianBlur: Initialization required.')
-  }
-  const result = new Float32Array(data.length)
-  const inputPtr = allocate_memory(data.length)
-  const outputPtr = allocate_memory(data.length)
+export const gaussianBlur = (instance: InitOutput | undefined, data: Float32Array, radius: number, blend: number): Promise<Float32Array> => {
+  return new Promise((resolve, reject) => {
+    if (!instance) {
+      return reject(new Error('gaussianBlur: Initialization required.'))
+    }
 
-  const input = new Float32Array(
-    instance.memory.buffer,
-    inputPtr,
-    data.length,
-  )
-  input.set(data)
+    try {
+      const result = new Float32Array(data.length)
+      const inputPtr = allocate_memory(data.length)
+      const outputPtr = allocate_memory(data.length)
 
-  gaussian_blur(inputPtr, outputPtr, data.length, radius, blend)
+      const input = new Float32Array(
+        instance.memory.buffer,
+        inputPtr,
+        data.length,
+      )
+      input.set(data)
 
-  const output = new Float32Array(
-    instance.memory.buffer,
-    outputPtr,
-    data.length,
-  )
-  result.set(output)
+      gaussian_blur(inputPtr, outputPtr, data.length, radius, blend)
 
-  free_memory(inputPtr, data.length)
-  free_memory(outputPtr, data.length)
+      const output = new Float32Array(
+        instance.memory.buffer,
+        outputPtr,
+        data.length,
+      )
+      result.set(output)
 
-  return result
+      free_memory(inputPtr, data.length)
+      free_memory(outputPtr, data.length)
+
+      resolve(result)
+    } catch (error) {
+      reject(error)
+    }
+  })
 }
 
-export const unsharpMask = (instance: InitOutput | undefined, data: Float32Array, amount: number, radius: number) => {
-  if (!instance) {
-    throw new Error('unsharpMask: Initialization required.')
-  }
-  const result = new Float32Array(data.length)
-  const size = Math.sqrt(data.length)
-  const inputPtr = allocate_memory(data.length)
-  const blurredPtr = allocate_memory(data.length)
-  const outputPtr = allocate_memory(data.length)
+export const unsharpMask = (instance: InitOutput | undefined, data: Float32Array, amount: number, radius: number): Promise<Float32Array> => {
+  return new Promise((resolve, reject) => {
+    if (!instance) {
+      return reject(new Error('unsharpMask: Initialization required.'))
+    }
 
-  const input = new Float32Array(
-    instance.memory.buffer,
-    inputPtr,
-    data.length,
-  )
-  input.set(data)
+    try {
+      const result = new Float32Array(data.length)
+      const inputPtr = allocate_memory(data.length)
+      const blurredPtr = allocate_memory(data.length)
+      const outputPtr = allocate_memory(data.length)
 
-  gaussian_blur(inputPtr, blurredPtr, data.length, radius, 1)
-  unsharp_mask(inputPtr, blurredPtr, outputPtr, size, size, amount)
+      const input = new Float32Array(
+        instance.memory.buffer,
+        inputPtr,
+        data.length,
+      )
+      input.set(data)
 
-  const output = new Float32Array(
-    instance.memory.buffer,
-    outputPtr,
-    data.length,
-  )
-  result.set(output)
+      gaussian_blur(inputPtr, blurredPtr, data.length, radius, 1)
+      unsharp_mask(inputPtr, blurredPtr, outputPtr, data.length, amount)
 
-  free_memory(inputPtr, data.length)
-  free_memory(blurredPtr, data.length)
-  free_memory(outputPtr, data.length)
+      const output = new Float32Array(
+        instance.memory.buffer,
+        outputPtr,
+        data.length,
+      )
+      result.set(output)
 
-  return result
+      free_memory(inputPtr, data.length)
+      free_memory(blurredPtr, data.length)
+      free_memory(outputPtr, data.length)
+
+      resolve(result)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+export const noise = (instance: InitOutput | undefined, data: Float32Array, amount: number, range: number): Promise<Float32Array> => {
+  return new Promise((resolve, reject) => {
+    if (!instance) {
+      return reject(new Error('noise: Initialization required.'))
+    }
+
+    try {
+      const result = new Float32Array(data.length)
+      const inputPtr = allocate_memory(data.length)
+      const outputPtr = allocate_memory(data.length)
+
+      const input = new Float32Array(
+        instance.memory.buffer,
+        inputPtr,
+        data.length,
+      )
+      input.set(data)
+
+      gen_noise(inputPtr, outputPtr, data.length, amount, range)
+
+      const output = new Float32Array(
+        instance.memory.buffer,
+        outputPtr,
+        data.length,
+      )
+      result.set(output)
+
+      free_memory(inputPtr, data.length)
+      free_memory(outputPtr, data.length)
+
+      resolve(result)
+    } catch (error) {
+      reject(error)
+    }
+  })
 }
