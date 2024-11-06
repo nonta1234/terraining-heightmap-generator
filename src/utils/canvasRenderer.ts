56e76f3fcc3d8590e67edbb96eee92927e172ff2
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2024-11-06 23:50:14 +0900

    update canvas renderer

--- src/utils/canvasRenderer.ts
+++ src/utils/canvasRenderer.ts
@@ -1,13 +1,18 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import { AnimationLoop, Model } from '@luma.gl/engine'
+import { Model } from '@luma.gl/engine'
 import { Buffer, clear, Texture2D } from '@luma.gl/webgl'
 
-export const renderCanvas = (canvas: HTMLCanvasElement, gl: WebGL2RenderingContext, data: Float32Array, min: number, max: number, scale: number) => {
+export const renderCanvas = (
+  canvas: HTMLCanvasElement,
+  gl: WebGL2RenderingContext,
+  data: Float32Array,
+  min: number,
+  max: number,
+  scale: number,
+) => {
   const size = Math.sqrt(data.length)
-  const textureScale = size / (size - 200)
-
-  gl.canvas.width = size - 200
-  gl.canvas.height = size - 200
+  canvas.width = size
+  canvas.height = size
+  gl.viewport (0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
 
   const texture = new Texture2D(gl, {
     data: data,
@@ -19,73 +24,64 @@ export const renderCanvas = (canvas: HTMLCanvasElement, gl: WebGL2RenderingConte
     parameters: {
       [gl.TEXTURE_MIN_FILTER]: gl.NEAREST,
       [gl.TEXTURE_MAG_FILTER]: gl.NEAREST,
+      [gl.TEXTURE_WRAP_S]: gl.CLAMP_TO_EDGE,
+      [gl.TEXTURE_WRAP_T]: gl.CLAMP_TO_EDGE,
     },
   })
 
-  const loop = new AnimationLoop({
-    // @ts-ignore
-    onInitialize({ gl }) {
-      const model = new Model(gl, {
-        vs: `#version 300 es
-          precision highp float;
+  const positionBuffer = new Buffer(
+    gl,
+    new Float32Array([
+      -1, -1,
+      -1, 1,
+      1, 1,
+      1, 1,
+      1, -1,
+      -1, -1,
+    ]),
+  )
 
-          in vec2 position;
-          uniform float uTextureScale;
-          out vec2 vTexCoord;
-
-          void main() {
-              vTexCoord = vec2((position.x + 1.0) * 0.5, 1.0 - 0.5 * (position.y + 1.0));
-              gl_Position = vec4(position.x * uTextureScale, position.y * uTextureScale, 0.0, 1.0);
-          }
-        `,
-        fs: `#version 300 es
-          precision highp float;
-
-          in vec2 vTexCoord;
-          uniform sampler2D uTexture;
-          uniform float uMin;
-          uniform float uMax;
-          uniform float uScale;
-          out vec4 fragColor;
-
-          void main() {
-              float elevation = texture(uTexture, vTexCoord).r;
-              float normalizedElevation = (elevation - uMin) / (uMax - uMin) * uScale ;
-              normalizedElevation = clamp(normalizedElevation, 0.0, 1.0);
-              fragColor = vec4(normalizedElevation, normalizedElevation, normalizedElevation, 1.0);
-          }
-        `,
-        uniforms: {
-          uTexture: texture,
-          uMin: min,
-          uMax: max,
-          uScale: scale,
-          uTextureScale: textureScale,
-        },
-        attributes: {
-          position: new Buffer(
-            gl,
-            new Float32Array([
-              -1, -1,
-              -1, 1,
-              1, 1,
-              1, 1,
-              1, -1,
-              -1, -1,
-            ]),
-          ),
-        },
-        vertexCount: 6,
-      })
-      return { model }
+  const model = new Model(gl, {
+    vs: `#version 300 es
+      precision highp float;
+      in vec2 position;
+      out vec2 vTexCoord;
+      void main() {
+          vTexCoord = vec2((position.x + 1.0) * 0.5, 1.0 - 0.5 * (position.y + 1.0));
+          gl_Position = vec4(position.x, position.y, 0.0, 1.0);
+      }
+    `,
+    fs: `#version 300 es
+      precision highp float;
+      in vec2 vTexCoord;
+      uniform sampler2D uTexture;
+      uniform float uMin;
+      uniform float uMax;
+      uniform float uScale;
+      out vec4 fragColor;
+      void main() {
+          float elevation = texture(uTexture, vTexCoord).r;
+          float normalizedElevation = (elevation - uMin) / (uMax - uMin) * uScale;
+          normalizedElevation = clamp(normalizedElevation, 0.0, 1.0);
+          fragColor = vec4(normalizedElevation, normalizedElevation, normalizedElevation, 1.0);
+      }
+    `,
+    uniforms: {
+      uTexture: texture,
+      uMin: min,
+      uMax: max,
+      uScale: scale,
     },
-
-    // @ts-ignore
-    onRender({ gl, model }) {
-      clear(gl, { color: [0, 0, 0, 1] })
-      model.draw()
+    attributes: {
+      position: positionBuffer,
     },
+    vertexCount: 6,
   })
 
-  loop.start({ canvas })
+  clear(gl, { color: [0, 0, 0, 1] })
+  model.draw()
+
+  texture.delete()
+  positionBuffer.delete()
+  model.delete()
 }
