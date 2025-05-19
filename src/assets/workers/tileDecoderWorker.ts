32ac9e4b100ed7ef71a14c6bcdbcd136d676c3fb
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2025-05-19 21:37:42 +0900

    fix gc

--- src/assets/workers/tileDecoderWorker.ts
+++ src/assets/workers/tileDecoderWorker.ts
@@ -1,8 +1,9 @@
 import * as Comlink from 'comlink'
 import { decode as decode_webp } from '@jsquash/webp'
 import initPng, { decode_png } from '~~/wasm/png_lib/pkg'
-import { decodeElevation } from '~/utils/elevation'
 import type { MapType } from '~/types/types'
+import { decodeElevation } from '~/utils/elevation'
+import { subdivideTile } from '~/processes/subdivideTile'
 
 type TileData = {
   arrBuffer: ArrayBuffer
@@ -31,12 +32,13 @@ class TileDecoderWorker {
         byteArray = new Uint8ClampedArray(imgData.data)
       } else {
         const arr = new Uint8Array(arrBuffer)
-        const byte = await decode_png({ data: arr })
-        byteArray = new Uint8ClampedArray(byte.data)
+        const imgData = await decode_png({ data: arr })
+        byteArray = new Uint8ClampedArray(imgData.data)
+        imgData.data = new Uint8Array(0)
       }
 
       const result = decodeElevation(byteArray)
-
+      byteArray = new Uint8ClampedArray(0)
       return Comlink.transfer(result, [result.buffer])
     } catch (error) {
       console.error('Decoding error:', error)
@@ -44,12 +46,19 @@ class TileDecoderWorker {
     }
   }
 
+  public async subdivideTile(data: Float32Array, count: number, margin: number, enhance: number, damping: number) {
+    try {
+      const result = subdivideTile(data, count, margin, enhance, damping)
+      return Comlink.transfer(result, [result.buffer])
+    } catch (error) {
+      console.error('Subdivision error:', error)
+      throw error
+    }
+  }
+
   public async cleanup() {
     try {
       this.wasmInitialized = false
-      if (globalThis.gc) {
-        globalThis.gc()
-      }
     } catch (error) {
       console.error('Cleanup error:', error)
       throw error
